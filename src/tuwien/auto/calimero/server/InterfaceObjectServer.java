987b449b2b94dc8de1e00037178d066cb10556d6
bmalinowsky <bmalinowsky@github.com>
2014-10-06 21:37:18 +0200

    Remove unnecessary checked exception from constructor

--- src/tuwien/auto/calimero/server/InterfaceObjectServer.java
+++ src/tuwien/auto/calimero/server/InterfaceObjectServer.java
@@ -103,8 +103,8 @@ public class InterfaceObjectServer implements PropertyAccess
 	// the list of interface objects is not expected to change frequently
 	private final List objects;
 
+	private final IosAdapter adapter = new IosAdapter();
 	private final PropertyClient client;
-	private final IosAdapter adapter;
 
 	private final boolean strictMode;
 
@@ -132,15 +132,21 @@ public class InterfaceObjectServer implements PropertyAccess
 	 *
 	 * @param strictPropertyMode <code>true</code> if server shall enforce strict mode,
 	 *        <code>false</code> otherwise
-	 * @throws KNXFormatException
 	 */
-	public InterfaceObjectServer(final boolean strictPropertyMode) throws KNXFormatException
+	public InterfaceObjectServer(final boolean strictPropertyMode)
 	{
 		strictMode = strictPropertyMode;
+		try {
+			client = new PropertyClient(adapter);
+		}
+		catch (final KNXFormatException e) {
+			// Don't propagate this checked exception type: it indicates that we requested an
+			// unsupported DPT. But the property client DPT is known and supported in the standard
+			// set of Calimero translators.
+			throw new KNXIllegalArgumentException("cannot create property client", e);
+		}
 
 		logger = LogManager.getManager().getLogService("Interface Object Server");
-		adapter = new IosAdapter();
-		client = new PropertyClient(adapter);
 
 		objects = new ArrayList();
 
