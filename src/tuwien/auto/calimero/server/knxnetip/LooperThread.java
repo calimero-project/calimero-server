58dece9a33855cd831f940846e71d94cf4c38fa8
bmalinowsky <bmalinowsky@github.com>
2017-01-25 21:45:37 +0100

    Closing data connections is not required (and not a task of the looper)

--- src/tuwien/auto/calimero/server/knxnetip/LooperThread.java
+++ src/tuwien/auto/calimero/server/knxnetip/LooperThread.java
@@ -1,6 +1,6 @@
 /*
     Calimero 2 - A library for KNX network access
-    Copyright (c) 2016 B. Malinowsky
+    Copyright (c) 2016, 2017 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -98,8 +98,6 @@ class LooperThread extends Thread
 				}
 				else {
 					server.logger.error("error setting up " + super.getName());
-					if (sc != null)
-						server.closeDataConnections(sc);
 				}
 			}
 		}
