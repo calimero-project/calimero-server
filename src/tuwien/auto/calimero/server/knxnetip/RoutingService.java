ad6f0f1b4f48cbabb0ae1cb3032663e58781d994
bmalinowsky <bmalinowsky@github.com>
2020-04-25 15:16:21 +0200

    Rename routing service

--- src/tuwien/auto/calimero/server/knxnetip/RoutingService.java
+++ src/tuwien/auto/calimero/server/knxnetip/RoutingService.java
@@ -1,6 +1,6 @@
 /*
     Calimero 2 - A library for KNX network access
-    Copyright (c) 2016, 2019 B. Malinowsky
+    Copyright (c) 2016, 2020 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -85,7 +85,7 @@ final class RoutingService extends ServiceLooper
 		@Override
 		public String getName()
 		{
-			return "KNXnet/IP routing service " + ctrlEndpt.getAddress().getHostAddress();
+			return "KNX IP routing service " + ctrlEndpt.getAddress().getHostAddress();
 		}
 
 		DatagramSocket getLocalDataSocket()
