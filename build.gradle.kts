4ccff1daf1a6701d1d6e7ee60b145db2936d8542
bmalinowsky <bmalinowsky@github.com>
2026-04-27 09:28:59 +0200

    Upgrade to versions 0.54.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 	`maven-publish`
 	signing
 	id("org.graalvm.buildtools.native") version "1.0.0"
-	id("com.github.ben-manes.versions") version "0.53.0"
+	id("com.github.ben-manes.versions") version "0.54.0"
 }
 
 repositories {
