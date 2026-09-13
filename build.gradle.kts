55b7a60315bf6ee2e08d06b5d727fa2a85964ae1
bmalinowsky <bmalinowsky@github.com>
2026-09-13 13:19:50 +0200

    Replace deprecated com.github.ben-manes.versions plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 	`maven-publish`
 	signing
 	id("org.graalvm.buildtools.native") version "1.1.12"
-	id("com.github.ben-manes.versions") version "0.62.0"
+	id("io.github.ben-manes.versions") version "0.62.0"
 }
 
 repositories {
